aeba8bebb35884fdc35e5909d721fe574f89b52f
McKinney <c-pamckinn@PA.LCL>
2023-07-17 16:29:30 -0400

    upgrade libs

--- src/js/constants.js
+++ src/js/constants.js
@@ -5,5 +5,3 @@ export const windowWidth = window.innerWidth || document.documentElement.clientW
 // Map & Controls
 export const homeCoords = [40.63189, -79.16747];
 export const initZoom = setInitialMapZoom(windowWidth);
-// esri api key
-export const esriKey = 'AAPK56c13fae1a6d4407a5d392981b9b50d2kwxUZMYHbCl1MyifP-JufC7lrC9K2TvvL93oR25h20j0cjvoAro30x-p0O27jPwS';
